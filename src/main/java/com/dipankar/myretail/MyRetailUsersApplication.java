2416514091f69d23c9686e0ff5a6cdffeaaee576
dipankar-datta <dattapro5@gmail.com>
2019-09-22 20:31:24 +0530

    Initial commit.

---
+++ src/main/java/com/dipankar/myretail/MyRetailUsersApplication.java
@@ -0,0 +1,13 @@
+package com.dipankar.myretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRetailUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRetailUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dipankar/myretail/MyRetailUsersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dipankar.myretail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyRetailUsersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
