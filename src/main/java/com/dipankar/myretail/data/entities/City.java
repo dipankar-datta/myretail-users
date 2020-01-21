f580841571ca935d224121bc4de9209052adc06e
Dipankar Datta <dattapro5@gmail.com>
2020-01-21 18:59:32 +0530

    Fixed State City mapping issue.

--- src/main/java/com/dipankar/myretail/data/entities/City.java
+++ src/main/java/com/dipankar/myretail/data/entities/City.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.*;
 
 @Data
 @Entity
@@ -16,6 +16,7 @@ import java.util.Date;
 public class City implements Serializable {
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -29,16 +30,14 @@ public class City implements Serializable {
     @Size(min = 2, max = 120, message = "City name should be between 2 and 120 characters")
     private String name;
 
-    //@Column(name = "created_by")
     @NotNull(message = "Created by field is required")
-    @OneToOne
-    @JoinColumn(name = "created_by", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by")
     private User createdBy;
 
-    //@Column(name = "updated_by")
     @NotNull(message = "Updated by field is required")
-    @OneToOne
-    @JoinColumn(name = "updated_by", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "updated_by")
     private User updatedBy;
 
     @Column(name = "creation_time")
@@ -46,6 +45,4 @@ public class City implements Serializable {
 
     @Column(name = "updation_time")
     private Date updationTime;
-
-
 }
--- src/main/java/com/dipankar/myretail/data/entities/State.java
+++ src/main/java/com/dipankar/myretail/data/entities/State.java
@@ -4,16 +4,20 @@ import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
-import java.util.List;
+import java.util.*;
 
 @Data
 @Entity
 @Table(name = "states")
 public class State implements Serializable {
 
+
+
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -27,11 +31,32 @@ public class State implements Serializable {
     @Size(min = 2, max = 120, message = "State name should be between 2 and 120 characters")
     private String name;
 
-    @ManyToMany
-    @JoinTable(
-            name = "state_city_map",
-            joinColumns = {@JoinColumn(name = "state")},
-            inverseJoinColumns = {@JoinColumn(name = "city")}
+    @NotNull(message = "Created by field is required")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by")
+    private User createdBy;
+
+    @NotNull(message = "Updated by field is required")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "updated_by")
+    private User updatedBy;
+
+    @Column(name = "creation_time")
+    private Date creationTime;
+
+    @Column(name = "updation_time")
+    private Date updationTime;
+
+    @OneToMany(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
     )
-    private List<City> cities;
+    @JoinTable(
+            name = "country_state_city_map",
+            joinColumns = @JoinColumn(name = "state", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "city", referencedColumnName = "id")
+    )
+    private Set<City> cities;
+
+
 }
